62c53c57eb10d584644ce2bfd7348096d9b7240c
Jaider-Duvan <jaiderduvan961@gmail.com>
2025-07-24 21:54:56 -0500

    Se actualiza contador

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 let contador = 0;
 
-document.getElementById("Incrementar").addEventListener("click",function(){
-    const 
+const contadorElemento = document.getElementById("contador");
+const btnIncrementar = document.getElementById("Incrementar");
+const btnDisminuir = document.getElementById("Disminuir");
+
+function actualizarContador() {
+    contadorElemento.textContent = contador;
+
+    if (contador === 10) {
+        contadorElemento.classList.add("color-cambio");
+    } else {
+        contadorElemento.classList.remove("color-cambio");
+    }
 }
+
+btnIncrementar.addEventListener("click", () => {
+    contador++;
+    actualizarContador();
+});
+
+btnDisminuir.addEventListener("click", () => {
+    contador--;
+    actualizarContador();
+});
